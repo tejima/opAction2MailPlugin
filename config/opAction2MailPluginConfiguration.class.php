0bcb3a3af1e19dd6838ddb423d3f1e05c4a732bb
手手嶋守 <tejima@gmail.com>
2011-02-10 23:05:25 +0900

    apply event Test Required.

--- config/opAction2MailPluginConfiguration.class.php
+++ config/opAction2MailPluginConfiguration.class.php
@@ -8,14 +8,54 @@ class opAction2MailPluginConfiguration extends sfPluginConfiguration
       'op_action.post_execute_message_sendToFriend',
       array($this,'sendToFriend')
     );
+    $this->dispatcher->connect(
+      'op_action.post_execute_communityEvent_create',
+      array($this,'event2mail')
+    );
+
+  }
+
+  public function event2mail($event){
+    if(version_compare(OPENPNE_VERSION, '3.5.0', '<=')){
+      require_once(dirname(__FILE__).'/dummyload.php');
+    }
+    $action = $event['actionInstance'];
+    $object = $action->getRoute()->getObject();
+    if ($object instanceof Community){
+    }else{
+      die("something wrong.");
+    }
+    $community_id = $object->id;
+    $cm_list = Doctrine_query::create()->from("CommunityMember cm")->where("community_id = ?",$community_id)->execute();
+
+    $data = $action->getRequestParameter('community_event');
+    $url = sfConfig::get('op_base_url');    
+    $message = <<<EOF
+{$data['open_date']['month']}/{$data['open_date']['day']} {$data['name']}イベントのお知らせ
+
+{$data['open_date']['month']}/{$data['open_date']['day']} {$data['name']}イベント開催のお知らせメールです。
+
+{$data['body']}
+
+イベントを開催するコミュニティの情報はこちら。
+{$url}community/{$community_id}
+EOF;
+    foreach($cm_list as $cm){
+      $member = Doctrine::getTable("Member")->find($cm->member_id);
+      $this->notifyMail($member,$message);
+    }
+    
+    $msg = "community_id:{$object->id}\n";
+    error_log("$msg",3,'/tmp/log');
   }
   public function sendToFriend($event){
-
     if(version_compare(OPENPNE_VERSION, '3.5.0', '<=')){
       require_once(dirname(__FILE__).'/dummyload.php');
     }
 
     $action = $event['actionInstance'];
+    
+
     if(sfRequest::POST != $action->getRequest()->getMethod()){
       return;
     }
